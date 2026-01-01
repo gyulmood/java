c27e8e03c3c736385902237ebc363f982d411510
gyulmood <gyulmood@gmail.com>
2026-01-01 21:10:02 +0900

    implicit conversion

--- src/DataType.java
+++ src/DataType.java
@@ -25,5 +25,18 @@ public class DataType {
         System.out.println("long 의 메모리 크기 : " + Long.BYTES + "byte (" + Long.SIZE + "bit)");
 
         System.out.println("char 의 메모리 크기 : " + Character.BYTES + "byte (" + Character.SIZE + "bit)");
+
+        int a = 4;
+        float b = 2.27F;
+
+        float c = a + b;
+        double d = a + b;
+
+        System.out.println("c = " + c); // 6.27
+        System.out.println("d = " + d); // 6.269999980926514
+
+        // a + b 는 implicit conversion으로 인하여 (float) a  + (float) b 로 계산된다.
+        // d는 double 형이 되면서 더 큰 정밀도를 가져 6.27이 아닌 값을 가진다
+        // (원인: 부동소숫점; 정해진 비트 수 안에 소수를 끼워넣어야 하는데 이 과정에서 오차가 생긴다.)
     }
 }
