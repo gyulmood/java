b5083090858e7767816cd476d4fb3fbfe7b590f0
gyulmood <gyulmood@gmail.com>
2026-01-06 21:32:42 +0900

    class && object

---
+++ src/Object/CalculateDemo.java
@@ -0,0 +1,17 @@
+package Object;
+
+public class CalculateDemo {
+
+    public static void main(String[] args) {
+        // c1 과 c2 는 각각 인스턴스.
+        // class 는 설계도 / 인스턴스는 설계도대로 만든 구체적인 객체
+        Calculator c1 = new Calculator();
+        c1.setOperands(10, 20);
+        c1.sum();
+        c1.avg();
+
+        Calculator c2 = new Calculator();
+        c2.setOperands(20, 40);
+        c2.avg();
+    }
+}
---
+++ src/Object/Calculator.java
@@ -0,0 +1,18 @@
+package Object;
+
+public class Calculator {
+    int a, b;
+
+    public void setOperands(int left, int right) {
+        this.a = left;
+        this.b = right;
+    }
+
+    public void sum() {
+        System.out.println(this.a + this.b);
+    }
+
+    public void avg() {
+        System.out.println((this.a + this.b) / 2);
+    }
+}
