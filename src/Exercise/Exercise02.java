b69590e14bf7ab2981a664274a91defb441329c2
gyulmood <gyulmood@gmail.com>
2026-01-13 22:30:32 +0900

    exercise

---
+++ src/Exercise/Exercise02.java
@@ -0,0 +1,12 @@
+package Exercise;
+
+public class Exercise02 {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = (++x) + (y--);
+        System.out.println("z = " + z);
+        System.out.println("y = " + y);
+    }
+}
---
+++ src/Exercise/Exercise03.java
@@ -0,0 +1,22 @@
+package Exercise;
+
+public class Exercise03 {
+
+    public static void main(String[] args) {
+        double x = 5.0;
+        double y = 0.0;
+
+        double z = x % y;
+
+        if (Double.isNaN(z)) {
+            System.out.println("0.0으로 나눌 수 없습니다.");
+        } else {
+            double result = z + 10;
+            System.out.println("결과: " + result);
+        }
+
+        y += 2;
+        double r = x % y;
+        System.out.println("r = " + r);
+    }
+}
